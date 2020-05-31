a4bbd964b4948839c3ddf560d4bb9aa8f52da229
filippilif155 <50518281+filippilif155@users.noreply.github.com>
2020-06-01 01:15:20 +0200

    udate domacinstvo js

--- census-household/index.js
+++ census-household/index.js
@@ -1,9 +1,9 @@
 let next = document.querySelector(".tick");
 let prev = document.querySelector(".back");
-
+​
 next.addEventListener("click", nextQuestion);
 let count=0;
-
+​
 function nextQuestion(e){
     count++;
     e.preventDefault();
@@ -15,15 +15,16 @@ function nextQuestion(e){
     }
     if (count === 5){
         openModalWindow();
+        count--;
     }
-
+​
     function openModalWindow(){
         let q1 =  "Broj ukućana: " + document.querySelector(".broj-ukucana").value;
         let q2 = "Izabrana opština: " + document.querySelector(".select-opstina").value;
         let q3 = "Naselje: " + document.querySelector(".ime-naselja").value + ", " + document.querySelector(".tip-naselja").value;
         let q4 = "Boravište: " + document.querySelector(".kuca").value;
         let q5 = "Prosječna zarada domaćinstva: " + document.querySelector(".zarada").value + '€';
-
+​
         let arrHelp = [q1, q2, q3, q4, q5];
         
         let modalW = document.createElement("div");
@@ -41,42 +42,40 @@ function nextQuestion(e){
             table.appendChild(row);
             
         }
-
-
+​
+​
         modalW.className = "modal-window";
         modalW.appendChild(table);
-
-        let sub = document.createElement("input");
-        sub.type = "submit";
-        sub.value = "Predaj odgovore";
-        sub.className = "submit";
-
+​
+​
+​
         let x = document.createElement("button");
         x.innerText = "X";
         x.className = "x";
-
-
+​
+​
         
         modalW.appendChild(x);
-        modalW.appendChild(sub);
+        document.querySelector(".submit").style.display = "inline-block";
         let mask = document.createElement("div");
         mask.className = "mask";
         x.addEventListener("click", (event) => {
             mask.style.display = "none";
   
             main.removeChild(modalW);
+            document.querySelector(".submit").style.display = "none";
           });
  
         let main = document.querySelector(".main");
         main.appendChild(mask);
         main.appendChild(modalW);
-
+​
         
     }
     
 }
 prev.addEventListener("click", prevQuestion);
-
+​
     function prevQuestion(e){
         if (count>=1){
             e.preventDefault();
@@ -92,7 +91,7 @@ prev.addEventListener("click", prevQuestion);
                 count--;
                 let curQ = document.querySelector(`.f${count+2}`);
                 curQ.style.display = "none";
-
+​
             }
             let prevQ = document.querySelector(`.f${count+1}`);
             prevQ.style.display = "block";
