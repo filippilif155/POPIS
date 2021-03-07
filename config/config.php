504556915dc6b601e227812d0dbb96cd25eb47c6
filippilif155 <50518281+filippilif155@users.noreply.github.com>
2021-03-07 15:41:56 +0100

    Test

--- config/config.php
+++ config/config.php
@@ -1,6 +1,6 @@
 <?php
 	define('ROOT_URL', '../home');
-	define('DB_HOST', 'x);
-	define('DB_USER', 'x);
-	define('DB_PASS', 'x);
-	define('DB_NAME', 'x);
+	define('DB_HOST', 'x' );
+	define('DB_USER', 'x' );
+	define('DB_PASS', 'x' );
+	define('DB_NAME', 'x' );
