a1a0ed41d4aee1a80d3326cdada1e5b7042e87d0
filippilif155 <50518281+filippilif155@users.noreply.github.com>
2021-03-07 15:40:41 +0100

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,6 +1,6 @@
 <?php
 	define('ROOT_URL', '../home');
-	define('DB_HOST', 'localhost');
-	define('DB_USER', 'filipm');
-	define('DB_PASS', 'filippilif21');
-	define('DB_NAME', 'filipm');
+	define('DB_HOST', 'x);
+	define('DB_USER', 'x);
+	define('DB_PASS', 'x);
+	define('DB_NAME', 'x);
